1deec9af0f125333e2e26b95fecffc0dc04dfcfd
Olexander63 <olexander63@gmail.com>
2021-03-29 18:28:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("start draw Cats");
+
+
+const drawCats = function (HowManyTimes) {
+    for (let i = 0; i < HowManyTimes; i++) {
+        console.log(i + " =^.^=");
+    }
+};
+
+drawCats(10); // Викликаємо 10 китотиків / Calling 10 Cats
+
